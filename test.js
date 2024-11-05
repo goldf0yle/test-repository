d4bcec48bf59b48b8c71e497dadac551450c2627
Leekyujin <69070926+goldf0yle@users.noreply.github.com>
2024-11-05 14:05:19 +0900

    test function from dev brach

--- test.js
+++ test.js
@@ -3,3 +3,11 @@
 const logTest = () => {
     console.log('test');
 }
+
+const conflictTestFn = ({id}) => {
+    if (typeof id === 'string') {
+        console.log('this is string type');
+    } else {
+        console.log('this is not string type');
+    }
+}
